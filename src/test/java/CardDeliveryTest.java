08964ebefb37894b5e0f5c46fe27baa33f8ca814
Родионова Анастасия Константиновна <a.rodionova@116332-mbp.local>
2022-04-14 00:00:05 +0300

    Fix tests

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -11,6 +13,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
+import static org.openqa.selenium.Keys.*;
 
 public class CardDeliveryTest {
 
@@ -18,16 +21,23 @@ public class CardDeliveryTest {
     @Test
     public void shouldOrderCardWithDelivery() {
         Configuration.holdBrowserOpen = true;
-    //    Configuration.browser = "firefox";
+        //    Configuration.browser = "firefox";
         open("http://localhost:9999/");
         $("[placeholder='Город']").setValue("Санкт-Петербург");
-        LocalDate currentDate = LocalDate.now().plusDays(3);
-        $("[placeholder='Дата встречи']").setValue("currentDate");
+
+        LocalDate date = LocalDate.now().plusDays(4);
+        //%02d - добавить ноль, если меньше 2х символов
+        String myDate = String.format("%02d.%02d.%d", date.getDayOfMonth(), date.getMonthValue(), date.getYear());
+
+        $("[data-test-id=date] input").sendKeys(chord(COMMAND, "a"), DELETE);
+        $("[placeholder='Дата встречи']").setValue(myDate);
         $("[name='name']").setValue("Чайковский Петр");
         $("[name='phone']").val("+79998887766");
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
-        $(withText("Успешно")).shouldBe(appear, Duration.ofSeconds(15));
+        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Успешно! Встреча успешно забронирована на " + myDate));
+
     }
 
     @Test
@@ -35,14 +45,14 @@ public class CardDeliveryTest {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
         $("[placeholder='Город']").setValue("Тв");
-        $$(".menu-item").filter(visible).last().click();
+        $(".input__menu").find(withText("Тверь")).click();
         $("[data-test-id=date] [value]").click();
 
         LocalDate dateOfCreationOfApplication = LocalDate.now();
         LocalDate dateOfMeeting = LocalDate.now().plusDays(7);
 
-        if (dateOfMeeting.getMonthValue()>dateOfCreationOfApplication.getMonthValue()
-                |dateOfMeeting.getYear()>dateOfCreationOfApplication.getYear()){
+        if (dateOfMeeting.getMonthValue() > dateOfCreationOfApplication.getMonthValue()
+                | dateOfMeeting.getYear() > dateOfCreationOfApplication.getYear()) {
             $(".calendar__arrow_direction_right[data-step='1']").click();
         }
         String dayToSearch = String.valueOf(dateOfMeeting.getDayOfMonth());
@@ -53,63 +63,6 @@ public class CardDeliveryTest {
         $("[data-test-id='agreement']").click();
         $(byText("Забронировать")).click();
         String stringDateInSuccessPopUp = dateOfMeeting.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-     //   $(withText("Успешно! Встреча успешно забронирована на " + stringDateInSuccessPopUp)).shouldBe(visible, Duration.ofSeconds(20));
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15))
-                .shouldHave(exactText("Успешно! Встреча успешно забронирована на " + stringDateInSuccessPopUp));
-    }
-
-    @Test
-    public void shouldFlipTheCalendarToTheNextMonth() {
-        Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999/");
-        $("[placeholder='Город']").setValue("Тв");
-        $$(".menu-item").filter(visible).last().click();
-        $("[data-test-id=date] [value]").click();
-
-        LocalDate dateOfCreationOfApplication = LocalDate.now();
-        LocalDate dateOfMeeting = LocalDate.now().plusDays(30);
-
-        if (dateOfMeeting.getMonthValue()>dateOfCreationOfApplication.getMonthValue()
-                |dateOfMeeting.getYear()>dateOfCreationOfApplication.getYear()){
-            $(".calendar__arrow_direction_right[data-step='1']").click();
-        }
-        String dayToSearch = String.valueOf(dateOfMeeting.getDayOfMonth());
-        $$("td.calendar__day").find(exactText(dayToSearch)).click();
-
-        $("[name='name']").setValue("Чайковский Петр");
-        $("[name='phone']").val("+79998887766");
-        $("[data-test-id='agreement']").click();
-        $(byText("Забронировать")).click();
-        String stringDateInSuccessPopUp = dateOfMeeting.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        //   $(withText("Успешно! Встреча успешно забронирована на " + stringDateInSuccessPopUp)).shouldBe(visible, Duration.ofSeconds(20));
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15))
-                .shouldHave(exactText("Успешно! Встреча успешно забронирована на " + stringDateInSuccessPopUp));
-    }
-
-    @Test
-    public void shouldFlipTheCalendarToTheNextYear() {
-        Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999/");
-        $("[placeholder='Город']").setValue("Тв");
-        $$(".menu-item").filter(visible).last().click();
-        $("[data-test-id=date] [value]").click();
-
-        LocalDate dateOfCreationOfApplication = LocalDate.now();
-        LocalDate dateOfMeeting = LocalDate.now().plusDays(365);
-
-        if (dateOfMeeting.getMonthValue()>dateOfCreationOfApplication.getMonthValue()
-                |dateOfMeeting.getYear()>dateOfCreationOfApplication.getYear()){
-            $(".calendar__arrow_direction_right[data-step='12']").click();
-        }
-        String dayToSearch = String.valueOf(dateOfMeeting.getDayOfMonth());
-        $$("td.calendar__day").find(exactText(dayToSearch)).click();
-
-        $("[name='name']").setValue("Чайковский Петр");
-        $("[name='phone']").val("+79998887766");
-        $("[data-test-id='agreement']").click();
-        $(byText("Забронировать")).click();
-        String stringDateInSuccessPopUp = dateOfMeeting.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        //   $(withText("Успешно! Встреча успешно забронирована на " + stringDateInSuccessPopUp)).shouldBe(visible, Duration.ofSeconds(20));
         $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15))
                 .shouldHave(exactText("Успешно! Встреча успешно забронирована на " + stringDateInSuccessPopUp));
     }
